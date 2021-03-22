3133a4b8f9f124ea33495cf598db06f0227f8c70
Eshitaashirodkar48 <74654166+Eshitaashirodkar48@users.noreply.github.com>
2021-03-22 15:56:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var database;
+var canvas;
+var gameState=0;
+var playerCount;
+var form, player, game;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  canvas=createCanvas(400,400);
+  game= new Game()
+  game.getState()
+  game.start() 
+}
+
+function draw(){
+  background("white");
+}
